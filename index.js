7c74c8f096cf1c93f8985380ac1ad98d3632f6bf
aleksander.sakhtakiy <azeric054@gmail.com>
2022-11-22 14:00:37 +0400

    add error catching

--- index.js
+++ index.js
@@ -16,10 +16,12 @@ const bot = new Bot(token);
 
 bot.on('message:text', async (ctx) => {
     try {
-        const res = await fetch('https://api.ipify.org').then(res => res.text());
+        const res = await fetch('https://api.ipify.org').then(res => res.text()).catch((err) => {
+          ctx.reply("cannot fetch api address ");
+        });
         ctx.reply("your ip address is " + res);
     } catch (error) {
-        console.log(error);
+        console.log('error has occured',error);
         if (error.message) {
             ctx.reply(error.message);
         } else {
